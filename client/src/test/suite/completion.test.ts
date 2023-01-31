0e9ecde383ef57329cfd08e197ea925ce3d8922b
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2023-01-31 19:21:09 +0900

    [DROOLS-7274] Add LSP client integration tests - WIP - Add CI for vscode extension test - Add a vscode extension test for code completion "package"

---
+++ client/src/test/suite/completion.test.ts
@@ -0,0 +1,43 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import * as vscode from 'vscode';
+import * as assert from 'assert';
+import { getDocUri, activate } from './helper';
+
+suite('Completion tests', () => {
+	const docUri = getDocUri('empty.drl');
+
+	test('Completes "package" at the beginning', async () => {
+		await testCompletion(docUri, new vscode.Position(0, 0), {
+			items: [
+				{ label: 'package', kind: vscode.CompletionItemKind.Keyword }
+			]
+		});
+	}).timeout(5000); // increase timeout from the default 2000ms because helper.activate waits 2000ms for server startup
+});
+
+async function testCompletion(
+	docUri: vscode.Uri,
+	position: vscode.Position,
+	expectedCompletionList: vscode.CompletionList
+) {
+	await activate(docUri);
+
+	// Executing the command `vscode.executeCompletionItemProvider` to simulate triggering completion
+	const actualCompletionList = (await vscode.commands.executeCommand(
+		'vscode.executeCompletionItemProvider',
+		docUri,
+		position
+	)) as vscode.CompletionList;
+
+	assert.ok(actualCompletionList.items.length >= expectedCompletionList.items.length);
+
+	expectedCompletionList.items.forEach((expectedItem) => {
+		const actualItem = actualCompletionList.items.find(item => item.label === expectedItem.label);
+		assert(actualItem);
+		assert.strictEqual(actualItem.kind, expectedItem.kind);
+	});
+}
